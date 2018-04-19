4117619e7d74244afa8c285325e4ee5c4f27f2b5
Rebecca Govert <becci.govert@gmail.com>
2018-04-19 19:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+   if (string.toLowerCase()) {
+    return "I can\'t hear you!"
   }
+  
+  
+  
   if (string==="I love you, Grandma.") {
     return "I love you, too."
     } 
